5b6cbae9e52d2e9c40f6cdbcc72be8cd4620cf54
Dimas Shofa Gunarso <55543999+Dimasshogun@users.noreply.github.com>
2021-12-13 23:56:52 +0700

    add initial minigames implementation
    - all minigames fully integrated with dialogue and quest
    - fix level manager and loading screen
    
    Co-authored-by: NaufalAulia <naufal7600@gmail.com>
    
    Merged from pull request (#2)

---
+++ Assets/Code/Scripts/Minigame/MinigameDrag.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+namespace Code.Scripts.Minigame
+{
+    public class MinigameDrag : MonoBehaviour, IDrag
+    {
+        public string answerValue;
+        public MinigameDropTarget target;
+
+        private Rigidbody _rb;
+
+        private Vector3 _initialPosition;
+        private Quaternion _initialRotation;
+        
+        private void Awake()
+        {
+            _initialPosition = transform.position;
+            _initialRotation = transform.rotation;
+            _rb = GetComponent<Rigidbody>();
+            _rb.isKinematic = true;
+        }
+
+        public void OnDragStart()
+        {
+            _rb.isKinematic = false;
+            _rb.constraints = RigidbodyConstraints.FreezeRotation;
+            
+            target = null;
+        }
+
+        public void OnDrag()
+        {
+        }
+        
+        public void OnDragEnd()
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (target == null)
+            {
+                transform.position = _initialPosition;
+                transform.rotation = _initialRotation;
+                _rb.isKinematic = true;
+                return;
+            }
+            
+            if (target.correctAnswer == answerValue)
+            {
+                target.isCorrect = true;
+                transform.position = target.transform.position;
+                transform.rotation = target.transform.rotation;
+                target.enabled = false;
+                MinigameManager.Instance.PlayCorrect();
+                _rb.isKinematic = true;
+                return;
+            }
+            transform.position = _initialPosition;
+            transform.rotation = _initialRotation;
+            MinigameManager.Instance.PlayIncorrect();
+            target = null;
+            _rb.isKinematic = true;
+            _rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
