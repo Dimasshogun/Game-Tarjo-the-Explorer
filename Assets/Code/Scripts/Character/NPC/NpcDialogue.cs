71ad9559e4ecabe7cd08092ff67ced427ea01694
NaufalAulia <naufal7600@gmail.com>
2022-01-10 01:12:14 +0700

    add random npc dialogue

--- Assets/Code/Scripts/Character/NPC/NpcDialogue.cs
+++ Assets/Code/Scripts/Character/NPC/NpcDialogue.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 namespace Code.Scripts.Character.NPC
@@ -6,5 +7,16 @@ namespace Code.Scripts.Character.NPC
     {
         public string characterName = "";
         public string entryNode = "";
+        [SerializeField] private YarnProgram randomDialogue;
+
+        private void Start()
+        {
+            if (randomDialogue != null)
+            {
+                var nodes = randomDialogue.GetProgram().Nodes.Keys.ToArray();
+                var selectRandom = Random.Range(0, nodes.Length);
+                entryNode = nodes[selectRandom];
+            }
+        }
     }
 }
